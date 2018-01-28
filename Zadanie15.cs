28f8ba4d743b7d441845a3ae9c6ee15b4112299b
Nagerrack <adamdebczak@gmail.com>
2018-01-28 23:04:16 +0100

    Add files via upload

--- Zadanie15.cs
+++ Zadanie15.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -13,6 +15,20 @@ namespace IO_final
 {
     class Zadanie15
     {
+        private static object lockObject = new object();
+
+        class Log
+        {
+            public static void CreateLog(string text)
+            {
+                lock (lockObject)
+                {
+                    string log = "[LOG] " + System.DateTime.Now + " " + text + Environment.NewLine;
+                    File.AppendAllText("logs.txt", log);
+                }
+            }
+        }
+
         class Server
         {
             #region Variables
@@ -91,60 +107,42 @@ namespace IO_final
                 {
                     throw (ex);
                 }
-                while (true && !ct.IsCancellationRequested)
+                while (!ct.IsCancellationRequested)
                 {
                     TcpClient client = await server.AcceptTcpClientAsync();
                     byte[] buffer = new byte[1024];
                     using (ct.Register(() => client.GetStream().Close()))
                     {
-                        client.GetStream().ReadAsync(buffer, 0, buffer.Length, ct).ContinueWith(
+                        await client.GetStream().ReadAsync(buffer, 0, buffer.Length, ct).ContinueWith(
                             async (t) =>
                             {
                                 int i = t.Result;
-                                while (true)
+                                
+                                while (!ct.IsCancellationRequested)
                                 {
-                                    client.GetStream().WriteAsync(buffer, 0, i, ct);
+                                    await client.GetStream().WriteAsync(buffer, 0, i, ct);
                                     try
                                     {
                                         i = await client.GetStream().ReadAsync(buffer, 0, buffer.Length, ct);
+                                        string text = Encoding.ASCII.GetString(buffer, 0, i);
+                                        Console.WriteLine("[S] Message: " + text);
+                                        Log.CreateLog(text);
+                                        Thread.Sleep(200);
                                     }
                                     catch
                                     {
                                         break;
                                     }
                                 }
-                            });
+                            }, ct);
                     }
                 }
             }
 
-            //public static async Task serverTask()
-            //{
-            //    TcpListener server = new TcpListener(IPAddress.Any, 2048);
-            //    server.Start();
-            //    while (true)
-            //    {
-            //        TcpClient client = await server.AcceptTcpClientAsync();
-            //        byte[]buffer =new byte[1024];
-            //        client.GetStream().ReadAsync(buffer, 0, buffer.Length).ContinueWith(
-            //            async (t) =>
-            //            {
-            //                int i = t.Result;
-            //                while (true)
-            //                {
-            //                    client.GetStream().WriteAsync(buffer, 0, i);
-            //                    i = await client.GetStream().ReadAsync(buffer, 0, buffer.Length);
-            //                }
-            //            }
-            //        );
-            //    }
 
-            //}
             public void RequestCancellation()
             {
                 cts.Cancel();
-                //serverTask.Wait();
-                //serverTask.Dispose();
                 server.Stop();
             }
 
@@ -156,7 +154,6 @@ namespace IO_final
             public void StopRunning()
             {
                 RequestCancellation();
-                //serverTask.Dispose();
             }
 
             #endregion
@@ -189,7 +186,7 @@ namespace IO_final
             public async Task<string> Ping(string message)
             {
                 byte[] buffer = new ASCIIEncoding().GetBytes(message);
-                client.GetStream().WriteAsync(buffer, 0, buffer.Length);
+                await client.GetStream().WriteAsync(buffer, 0, buffer.Length);
                 buffer = new byte[1024];
                 var t = await client.GetStream().ReadAsync(buffer, 0, buffer.Length);
                 return Encoding.UTF8.GetString(buffer, 0, t);
@@ -201,9 +198,13 @@ namespace IO_final
                 bool done = false;
                 while (!done)
                 {
-                    if (token.IsCancellationRequested)
-                        done = true;
                     messages.Add(await Ping(message));
+                    if (token.IsCancellationRequested)
+                    {
+                        done = true;
+                        Console.WriteLine("Cancelled");
+                    }
+
                 }
                 return messages;
             }
@@ -211,8 +212,54 @@ namespace IO_final
             #endregion
         }
 
-        public void Zadanie48()
+        public static async Task serverTask()
         {
+            TcpListener server = new TcpListener(IPAddress.Any, 2048);
+            server.Start();
+            while (true)
+            {
+                TcpClient client = await server.AcceptTcpClientAsync();
+                byte[] buffer = new byte[1024];
+               
+                await client.GetStream().ReadAsync(buffer, 0, buffer.Length).ContinueWith(
+                    async (t) =>
+                    {
+                        int i = t.Result;
+
+                        while (true)
+                        {
+                            
+                            await client.GetStream().WriteAsync(buffer, 0, i);
+                            i = await client.GetStream().ReadAsync(buffer, 0, buffer.Length);
+                            Console.WriteLine("[S] Message: " + Encoding.ASCII.GetString(buffer, 0, i));
+                        }
+                    }
+                );
+            }
+        }
+
+        public static async Task clientTask(int clientID)
+        {
+            string message = "[Client " + clientID + "] message";
+
+            TcpClient client = new TcpClient();
+            client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2048));
+
+           
+            byte[] buffer = new ASCIIEncoding().GetBytes(message);
+
+            await client.GetStream().WriteAsync(buffer, 0, buffer.Length);
+            buffer = new byte[1024];
+
+            int i = await client.GetStream().ReadAsync(buffer, 0, buffer.Length);
+            Console.WriteLine("[C] " + Encoding.ASCII.GetString(buffer,0,i));
+            
+        }
+
+
+        public static void Start()
+        {
+            File.Delete("logs.txt");
             Server s = new Server();
             s.Run();
             Client c1 = new Client();
@@ -224,13 +271,16 @@ namespace IO_final
             CancellationTokenSource ct1 = new CancellationTokenSource();
             CancellationTokenSource ct2 = new CancellationTokenSource();
             CancellationTokenSource ct3 = new CancellationTokenSource();
-            var client1T = c1.keepPinging("message", ct1.Token);
-            var client2T = c2.keepPinging("message", ct2.Token);
-            var client3T = c3.keepPinging("message", ct3.Token);
-            ct1.CancelAfter(2000);
-            ct2.CancelAfter(3000);
-            ct3.CancelAfter(4000);
-            Task.WaitAll(new Task[] {client1T, client2T, client3T});
+            var client1T = c1.keepPinging("message1", ct1.Token);
+            var client2T = c2.keepPinging("message2", ct2.Token);
+            var client3T = c3.keepPinging("message3", ct3.Token);
+
+            ct1.CancelAfter(500);
+            ct2.CancelAfter(1000);
+            ct3.CancelAfter(1500);
+
+            Task.WaitAll(client1T, client2T, client3T);
+            Console.WriteLine("WaitAll");
             s.StopRunning();
         }
     }
