d842264c05c0ab7f25403016563673007c848084
Nagerrack <adamdebczak@gmail.com>
2018-01-28 15:07:11 +0100

    Add files via upload

--- Zadanie9.cs
+++ Zadanie9.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,7 +15,7 @@ namespace IO_final
     {
         public delegate void MatMulCompletedEventHandler(object sender, MatMulCompletedEventArgs e);
 
-        private delegate void WorkerEventHandler(double[] mat1, double[] mat2, int size, AsyncOperation asyncOp);
+        private delegate void WorkerEventHandler(double[][] mat1, double[][] mat2, AsyncOperation asyncOp);
 
         public event MatMulCompletedEventHandler MatMulCompleted;
         private SendOrPostCallback onCompletedCallback;
@@ -33,7 +35,22 @@ namespace IO_final
             }
         }
 
-        void Completion(int size, double[] mat, Exception ex, bool cancelled, AsyncOperation ao)
+        public static double[][] RandomMat(int size)
+        {
+            Random r = new Random();
+            double[][] mat = new double[size][];
+            for (int i = 0; i < size; i++)
+            {
+                mat[i] = new double[size];
+                for (int j = 0; j < size; j++)
+                {
+                    mat[i][j] = r.NextDouble() * 100;
+                }
+            }
+            return mat;
+        }
+
+        void Completion(int size, double[][] mat, Exception ex, bool cancelled, AsyncOperation ao)
         {
         }
 
@@ -42,27 +59,42 @@ namespace IO_final
             return userState[TaskID] == null;
         }
 
-        void CalculateWorker(double[] mat1, double[] mat2, int size, AsyncOperation asyncOp)
+        void CalculateWorker(double[][] mat1, double[][] mat2, AsyncOperation asyncOp)
         {
-            double mat3 = new double(); //funkcja do mnozenia;
+            double[][] mat3 = MatMul(mat1, mat2);
             lock (userState.SyncRoot)
             {
                 userState.Remove(asyncOp.UserSuppliedState);
             }
-            object[] tabs = {mat1, mat2, mat3};
-            MatMulCompletedEventArgs e = new MatMulCompletedEventArgs(tabs, null, false, asyncOp.UserSuppliedState);
+            object[] mats = {mat1, mat2, mat3};
+            MatMulCompletedEventArgs e = new MatMulCompletedEventArgs(mats, null, false, asyncOp.UserSuppliedState);
             asyncOp.PostOperationCompleted(onCompletedCallback, e);
         }
 
-        double getVal(double[] mat, int column, int row, int size)
+        double[][] MatMul(double[][] mat1, double[][] mat2)
         {
+            double[][] mat3 = new double[mat1.Length][];
+            for (int i = 0; i < mat1.Length; i++)
+            {
+                mat3[i] = new double[mat1.Length];
+            }
+
+            for (int i = 0; i < mat1.Length; i++)
+            {
+                for (int j = 0; j < mat1.Length; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < mat1.Length; k++)
+                    {
+                        sum += mat1[i][k] * mat2[k][j];
+                    }
+                    mat3[i][j] = sum;
+                }
+            }
+            return mat3;
         }
 
-        double MatMul(double[] mat1, double[] mat2, int size)
-        {
-        }
-
-        public virtual void MatMulAsync(double[] mat1, double[] mat2, int size, object TaskID)
+        public virtual void MatMulAsync(double[][] mat1, double[][] mat2, object TaskID)
         {
             AsyncOperation ao = AsyncOperationManager.CreateOperation(TaskID);
             lock (userState.SyncRoot)
@@ -74,8 +106,7 @@ namespace IO_final
                 userState[TaskID] = ao;
             }
             WorkerEventHandler worker = new WorkerEventHandler(CalculateWorker);
-            //!!!!!
-            worker.BeginInvoke(mat1, mat2, 1, ao, null, null);
+            worker.BeginInvoke(mat1, mat2, ao, null, null);
         }
 
         public void CancelAsync(object TaskID)
@@ -89,30 +120,63 @@ namespace IO_final
                 }
             }
         }
+
+        public static void Print(double[][] mat)
+        {
+            foreach (var i in mat)
+            {
+                foreach (var j in i)
+                {
+                    Console.Write(Math.Round(j,2) + " ");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 
     class MatMulCompletedEventArgs : AsyncCompletedEventArgs
     {
-        public int[] Array1 { get; }
-        public int[] Array2 { get; }
-        public int[]Array3 { get; }
+        public double[][] Array1 { get; }
+        public double[][] Array2 { get; }
+        public double[][] Array3 { get; }
 
         public MatMulCompletedEventArgs(object Arrays, Exception e, bool cancelled, object state)
             : base(e, cancelled, state)
         {
-            Array1 = ((int[][]) Arrays)[0];
-            Array2 = ((int[][]) Arrays)[1];
-            Array3 = ((int[][]) Arrays)[2];
+            Array1 = (double[][]) ((object[]) Arrays)[0];
+            Array2 = (double[][]) ((object[]) Arrays)[1];
+            Array3 = (double[][]) ((object[]) Arrays)[2];
         }
     }
 
     class Zadanie9
     {
-    }
+        public static void Start()
+        {
+            int TaskID = 1;
+            MatMulCalculator mmc = new MatMulCalculator();
+            mmc.MatMulCompleted += onCompletion;
 
-    //class MatMulCalculator
-    //{
-    //    private delegate void MatMulCompleted;
-    //    private HybridDictionary operations = new HybridDictionary();
-    //}
+            for (int i = 0; i < 2; i++)
+            {
+                double[][] mat1 = MatMulCalculator.RandomMat(3);
+                Thread.Sleep(50);
+                double[][] mat2 = MatMulCalculator.RandomMat(3);
+                mmc.MatMulAsync(mat1,mat2,TaskID);
+                TaskID++;
+                
+            }
+            Thread.Sleep(1000);
+        }
+
+        public static void onCompletion(object sender, MatMulCompletedEventArgs e)
+        {
+            Console.WriteLine("Mat1:");
+            MatMulCalculator.Print(e.Array1);
+            Console.WriteLine("Mat2:");
+            MatMulCalculator.Print(e.Array2);
+            Console.WriteLine("Mat3:");
+            MatMulCalculator.Print(e.Array3);
+        }
+    }
 }
